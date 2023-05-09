49c506fb55f98995d9ab618524c9e5f7b06704fa
JingWei-S <weijing.524@gmail.com>
2023-05-09 21:17:09 +1000

    add the click feature

--- src/Components/GamePage.js
+++ src/Components/GamePage.js
@@ -2,10 +2,32 @@ import ImageComponent from "./ImageComponent";
 const GamePage = (props) => {
   const { img_src } = props; // local image first
 
-  const BOX = [[400, 1480], [480, 1540]];
+  const BOX = [
+    {
+      name: "Flashy",
+      pos: [
+        [400, 1480],
+        [480, 1540],
+      ],
+    },
+    {
+      name: "Whity",
+      pos: [
+        [1345, 290],
+        [1405, 365],
+      ],
+    },
+    {
+      name: "Red Batman",
+      pos: [
+        [990, 1665],
+        [1040, 1770],
+      ],
+    },
+  ];
 
   return (
-    <div>
+    <div className="gameImage">
       {/* <img src={process.env.PUBLIC_URL+img_src} alt="game image" onClick={clickCoord} /> */}
       <ImageComponent img_src={img_src} box={BOX} />
     </div>
--- src/Components/ImageComponent.js
+++ src/Components/ImageComponent.js
@@ -1,25 +1,45 @@
+import { useState } from "react";
+
 const ImageComponent = (props) => {
   const { img_src, box } = props; // local image first
+
+  const [ clickPos, setClickPos ] = useState([0, 0]);
 
   const clickCoord = (e) => {
     const x = e.pageX;
     const y = e.pageY;
     console.log("Clicked position x: " + x + ", y: " + y);
-    checkInBound(x, y);
+    // checkInBound(x, y);
+    // get the name selection button column
+    const selectCol = document.querySelector(".selectNameCol");
+    selectCol.style.display = "flex";
+    selectCol.style.top = y + "px";
+    selectCol.style.left = x + "px";
+    setClickPos([x, y]);
   };
 
-  const checkInBound = (x, y) => {
-    const minX = box[0][0];
-    const maxX = box[1][0];
-    const minY = box[0][1];
-    const maxY = box[1][1];
-    console.log(box)
+  const checkChar = (e) => {
+    const charName = e.target.textContent;
+    const clickPoint = clickPos;
+    // console.log(e.target.textContent);
+    checkInBound(clickPoint, charName);
+  };
+
+  const checkInBound = (clickPoint, charName) => {
+    const item = box.find(el => el.name === charName);
+    console.log(item.pos)
+    const x = clickPoint[0];
+    const y = clickPoint[1];
+    const minX = item.pos[0][0];
+    const maxX = item.pos[1][0];
+    const minY = item.pos[0][1];
+    const maxY = item.pos[1][1];
     if ((x >= minX) & (x <= maxX) & (y >= minY) & (y <= maxY)) {
       console.log("You clicked the character!");
     } else {
         console.log('Nooooo')
     }
-  };
+  }
 
   return (
     <div>
@@ -28,6 +48,19 @@ const ImageComponent = (props) => {
         alt="game image"
         onClick={clickCoord}
       />
+      {/* <div className="selectNameCol"> */}
+      <ul className="selectNameCol">
+        <li>
+          <button onClick={checkChar}>Flashy</button>
+        </li>
+        <li>
+          <button onClick={checkChar}>Whity</button>
+        </li>
+        <li>
+          <button onClick={checkChar}>Red Batman</button>
+        </li>
+      </ul>
+      {/* </div> */}
     </div>
   );
 };
